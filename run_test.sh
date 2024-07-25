9a385d4199547defae73a07da50e2663816217b3
Tobias Linker <tobilink1@t-online.de>
2024-07-25 17:20:42 +0200

    Fixed use of variable XCL_EMULATION_MODE

--- run_test.sh
+++ run_test.sh
@@ -4,10 +4,25 @@
 for VAR in "${!FPGA_PATH2EMU_@}"; do mkdir -p ${!VAR}; done
 
 echo "[INFO] Running test for mode: ${XCL_EMULATION_MODE_CHANGED}"
-export EMCONFIG_PATH=${FPGA_PATH2CONF}
-${FPGA_PATH2EMU_BIN}/app_${XCL_EMULATION_MODE_CHANGED} \
-  --xclbin ${FPGA_PATH2EMU_BIN}/kernels.xclbin \
-#  --treename ${XCL_EMULATION_MODE_CHANGED} \
+
+if [[ "${XCL_EMULATION_MODE_CHANGED}" = "hw" ]]
+then
+  unset XCL_EMULATION_MODE
+  export EMCONFIG_PATH=${FPGA_PATH2CONF}
+  ${FPGA_PATH2EMU_BIN}/app_${XCL_EMULATION_MODE_CHANGED} \
+    --xclbin ${FPGA_PATH2EMU_BIN}/kernels.xclbin
+elif [[ "${XCL_EMULATION_MODE_CHANGED}" = "hw_emu" || "${XCL_EMULATION_MODE_CHANGED}" = "sw_emu" ]]
+then
+  if [ -z ${XCL_EMULATION_MODE} ]; then
+    export XCL_EMULATION_MODE="${XCL_EMULATION_MODE_CHANGED}"
+  fi
+  export EMCONFIG_PATH=${FPGA_PATH2CONF}
+  ${FPGA_PATH2EMU_BIN}/app_${XCL_EMULATION_MODE_CHANGED} \
+    --xclbin ${FPGA_PATH2EMU_BIN}/kernels.xclbin \
+  --treename ${XCL_EMULATION_MODE_CHANGED}
+else
+  echo "[ERROR] Wrong emulation mode: ${XCL_EMULATION_MODE_CHANGED}"
+fi
 
 mv *.csv ${FPGA_PATH2EMU_SUM}/
 mv *.run_summary ${FPGA_PATH2EMU_SUM}/

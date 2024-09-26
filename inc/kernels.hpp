3b40f918e7154a75fa9487b8876e8a23687564db
Tobias Linker <tobilink1@t-online.de>
2024-09-26 18:09:48 +0200

    Input data structure changed to full graph

--- inc/kernels.hpp
+++ inc/kernels.hpp
@@ -4,11 +4,12 @@
 
 #define MAX_TOTAL_NODES 524288
 #define MAX_EDGES 16
+#define MAX_FULL_GRAPH_EDGES 256
 #define MAX_COMPONENT_SIZE 128
 #define MAX_TRUE_NODES 32768
-// #define CSV_FILE "dat/dummy.csv"
+#define CSV_FILE "dat/dummy.csv"
 // #define CSV_FILE "dat/dummy_long.csv"
-#define CSV_FILE "dat/event005001514.csv"
+// #define CSV_FILE "dat/event005001514.csv"
 // #define MAX_NODES 12
 // #define MAX_CONNECTIONS 4
 // #define MAX_COMPONENT_SIZE 8
@@ -20,7 +21,7 @@ struct node_information{
 };
 
 extern "C" {
-  void CCL(unsigned int* in_edge_from, unsigned int* in_edge_to, float* in_scores, unsigned int* io_graph, unsigned int* io_lookup, unsigned int* io_lookup_filter, unsigned int* out_labels, unsigned int num_edges, unsigned int num_nodes);
+  void CCL(unsigned int* in_full_graph, float* in_scores, unsigned int* io_graph, unsigned int* io_lookup, unsigned int* io_lookup_filter, unsigned int* out_labels, unsigned int num_edges, unsigned int num_nodes);
   // void CCL(unsigned int* in_edge_from, unsigned int* in_edge_to, float* in_scores, unsigned int* out_labels, unsigned int num_edges, unsigned int num_nodes);
   // void CCL(unsigned int* in_edge_from, unsigned int* in_edge_to, float* in_scores, node_information* out_node_info, int* out_graph, int num_edges, int num_nodes);
 }
--- src/host_double_lookup.cpp
+++ src/host_double_lookup.cpp
@@ -153,16 +153,18 @@ int main (int argc, char ** argv){
   //
   // Calculate size of vector in bytes
   //
-  size_t size_full_graph_byte = sizeof(unsigned int) * num_nodes * MAX_FULL_GRAPH_EDGES;
-  size_t size_scores_byte = sizeof(float) * num_edges * 2;
+  size_t size_edges_byte = sizeof(unsigned int) * num_edges;     // size of edge_from and edge_to -> int
+  size_t size_scores_byte = sizeof(float) * num_edges;  // size of scores -> float
+  size_t size_labels_byte = sizeof(unsigned int) * num_nodes;    // size of labels -> int
+  std::cout << "[INFO] Size of egdes:  " << size_edges_byte << " Bytes" << std::endl;
+  std::cout << "[    ] Size of scores: " << size_scores_byte << " Bytes" << std::endl;
+  std::cout << "[    ] Size of labels: " << size_labels_byte << " Bytes" << std::endl;
+  // ##########################
   size_t size_graph_byte = sizeof(unsigned int) * MAX_TRUE_NODES * MAX_EDGES;
   size_t size_lookup_byte = sizeof(unsigned int) * MAX_TOTAL_NODES;
-  size_t size_labels_byte = sizeof(unsigned int) * num_nodes;
-  std::cout << "[INFO] Size of full graph:  " << size_full_graph_byte << " Bytes" << std::endl;
-  std::cout << "[    ] Size of scores: " << size_scores_byte << " Bytes" << std::endl;
+  // ##########################
   std::cout << "[    ] Size of graph: " << size_graph_byte << " Bytes" << std::endl;
   std::cout << "[    ] Size of lookup: " << size_lookup_byte << " Bytes" << std::endl;
-  std::cout << "[    ] Size of labels: " << size_labels_byte << " Bytes" << std::endl;
   
   //
   // Define Kernel Function to be executed on Device
@@ -173,69 +175,61 @@ int main (int argc, char ** argv){
   // Allocate Buffer in Global Memory
   //
   std::cout << "[INFO] Allocate Buffer in Global Memory" << std::endl;
-  auto buffer_in_full_graph    = xrt::bo(targetDevice, size_full_graph_byte, krnl.group_id(0));    // gourp_id must fit to HBM-bank in u280.cfg
-  auto buffer_in_scores     = xrt::bo(targetDevice, size_scores_byte, krnl.group_id(1));
-  auto buffer_inout_graph = xrt::bo(targetDevice, size_graph_byte, krnl.group_id(2));
-  auto buffer_inout_lookup = xrt::bo(targetDevice, size_lookup_byte, krnl.group_id(3));
-  auto buffer_inout_lookup_filter = xrt::bo(targetDevice, size_lookup_byte, krnl.group_id(4));
-  auto buffer_out_labels    = xrt::bo(targetDevice, size_labels_byte, krnl.group_id(5));
+  auto buffer_in_edge_from  = xrt::bo(targetDevice, size_edges_byte, krnl.group_id(0));
+  auto buffer_in_edge_to    = xrt::bo(targetDevice, size_edges_byte, krnl.group_id(1));
+  auto buffer_in_scores     = xrt::bo(targetDevice, size_scores_byte, krnl.group_id(2));
+  auto buffer_out_labels    = xrt::bo(targetDevice, size_labels_byte, krnl.group_id(6));      // 6 because of order of idices and fitting to HBM-bank in u280.cfg
 
-  auto map_in_full_graph     = buffer_in_full_graph.map<unsigned int*>();
+  auto map_in_edge_from   = buffer_in_edge_from.map<unsigned int*>();
+  auto map_in_edge_to     = buffer_in_edge_to.map<unsigned int*>();
   auto map_in_scores      = buffer_in_scores.map<float*>();
-  auto map_inout_graph    = buffer_inout_graph.map<unsigned int*>();
-  auto map_inout_lookup    = buffer_inout_lookup.map<unsigned int*>();
-  auto map_inout_lookup_filter    = buffer_inout_lookup.map<unsigned int*>();
   auto map_out_labels     = buffer_out_labels.map<unsigned int*>();
+  // ##########################
+  auto buffer_inout_graph = xrt::bo(targetDevice, size_graph_byte, krnl.group_id(3));         // 3 because of order of idices and fitting to HBM-bank in u280.cfg
+  auto map_inout_graph    = buffer_inout_graph.map<unsigned int*>();
+  auto buffer_inout_lookup = xrt::bo(targetDevice, size_lookup_byte, krnl.group_id(4));       // 4 because of order of idices and fitting to HBM-bank in u280.cfg
+  auto map_inout_lookup    = buffer_inout_lookup.map<unsigned int*>();
+  auto buffer_inout_lookup_filter = xrt::bo(targetDevice, size_lookup_byte, krnl.group_id(5));       // 5 because of order of idices and fitting to HBM-bank in u280.cfg
+  auto map_inout_lookup_filter    = buffer_inout_lookup.map<unsigned int*>();
+  // ##########################
 
   //set to const 0 for output only -> necessary?
-  std::fill(map_in_full_graph, map_in_full_graph + num_nodes * MAX_FULL_GRAPH_EDGES, 0);
+  std::fill(map_out_labels, map_out_labels + num_nodes, -1);
+  // ##########################
   std::fill(map_inout_graph, map_inout_graph + MAX_TRUE_NODES * MAX_EDGES, 0);
   std::fill(map_inout_lookup, map_inout_lookup + MAX_TOTAL_NODES, 0);
   std::fill(map_inout_lookup_filter, map_inout_lookup + MAX_TOTAL_NODES, 0);
-  std::fill(map_out_labels, map_out_labels + num_nodes, -1);
+  // ##########################
 
   //
   // Fill input buffers with test data from CSV
   //
-  std::cout << "[INFO] Fill full graph data structure with data from CSV-file" << std::endl;
-  float score_table[num_nodes * MAX_FULL_GRAPH_EDGES];
-  // set all indicators that give the number of connections of that node to 0
-  for(unsigned int i = 0; i < num_nodes ; i++)
-    map_in_full_graph[i * MAX_FULL_GRAPH_EDGES] = 0;
-  // fill tables with connections and scores of the edges and keep the number of connections for each node up to date
+  std::cout << "[INFO] Fill input Buffers with data from CSV-file" << std::endl;
   for(unsigned int i = 0; i < num_edges ; i++){
-    map_in_full_graph[edge_from[i] * MAX_FULL_GRAPH_EDGES + map_in_full_graph[edge_from[i] * MAX_FULL_GRAPH_EDGES] + 1] = edge_to[i];
-    score_table[edge_from[i] * MAX_FULL_GRAPH_EDGES + map_in_full_graph[edge_from[i] * MAX_FULL_GRAPH_EDGES]] = scores[i];
-    map_in_full_graph[edge_from[i] * MAX_FULL_GRAPH_EDGES]++;
-    map_in_full_graph[edge_to[i] * MAX_FULL_GRAPH_EDGES + map_in_full_graph[edge_to[i] * MAX_FULL_GRAPH_EDGES] + 1] = edge_from[i];
-    score_table[edge_to[i] * MAX_FULL_GRAPH_EDGES + map_in_full_graph[edge_to[i] * MAX_FULL_GRAPH_EDGES]] = scores[i];
-    map_in_full_graph[edge_to[i] * MAX_FULL_GRAPH_EDGES]++;
-  }
-  // create a stream of scores out of the score-table
-  unsigned int edge = 0;
-  for(unsigned int row = 0; row < num_nodes ; row++){
-    for(unsigned int i = 0; i < map_in_full_graph[row * MAX_FULL_GRAPH_EDGES] ; i++){
-      map_in_scores[edge] = score_table[row * MAX_FULL_GRAPH_EDGES + i];
-      edge++;
-    }
+    map_in_edge_from[i] = edge_from[i];
+    map_in_edge_to[i] = edge_to[i];
+    map_in_scores[i] = scores[i];    
   }
 
   //
   // Synchronize input buffer data to device global memory
   //
   std::cout << "[INFO] Synchronize input buffer data to device global memory" << std::endl;
-  buffer_in_full_graph.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  buffer_in_edge_from.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  buffer_in_edge_to.sync(XCL_BO_SYNC_BO_TO_DEVICE);
   buffer_in_scores.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  // ##########################
   buffer_inout_graph.sync(XCL_BO_SYNC_BO_TO_DEVICE);
   buffer_inout_lookup.sync(XCL_BO_SYNC_BO_TO_DEVICE);
   buffer_inout_lookup_filter.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+  // ##########################
 
   //
   // Execute Kernel
   //
   std::cout << "[INFO] Execute Kernel" << std::endl;
   auto start = std::chrono::system_clock::now();
-  auto run = krnl(buffer_in_full_graph, buffer_in_scores, buffer_inout_graph, buffer_inout_lookup, buffer_inout_lookup_filter, buffer_out_labels, num_edges, num_nodes);
+  auto run = krnl(buffer_in_edge_from, buffer_in_edge_to, buffer_in_scores, buffer_inout_graph, buffer_inout_lookup, buffer_inout_lookup_filter, buffer_out_labels, num_edges, num_nodes);
   run.wait();
   auto end = std::chrono::system_clock::now();
 
